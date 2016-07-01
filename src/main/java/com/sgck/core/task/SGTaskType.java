b670715f205f42fd9180a46c862e0c2f8ee843b4
ShenguyunSDK <it@shenguyun.com>
2016-07-01 17:38:08 +0800

    初始化导入

---
+++ src/main/java/com/sgck/core/task/SGTaskType.java
@@ -0,0 +1,7 @@
+package com.sgck.core.task;
+
+public class SGTaskType {
+	public static final int BACKGROUND_TASK = 0;
+	public static final int DAU_TASK = 1;
+	public static final int CLIENT_TASK = 2;
+}
